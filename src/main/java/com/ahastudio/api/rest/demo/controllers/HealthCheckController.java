885e663e22c7a4548b620333e5bb8ba87b8682e9
smilejakdu <ash982416@gmail.com>
2023-12-13 22:27:58 +0900

    health

---
+++ src/main/java/com/ahastudio/api/rest/demo/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.ahastudio.api.rest.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "ok";
+    }
+}
